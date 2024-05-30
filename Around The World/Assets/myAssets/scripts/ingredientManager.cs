40b9e702494c77bd6483e48c92f0398e1bda89af
emilyhhayes <20hayemi@ths.school.nz>
2024-05-30 12:26:43 +1200

    get recipe manager working

--- Around The World/Assets/myAssets/scripts/ingredientManager.cs
+++ Around The World/Assets/myAssets/scripts/ingredientManager.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class ingredientManager : MonoBehaviour
 {
     public GameObject[] hideIngredients;
+
     [SerializeField] private recipeListSO recipeListSO;
     private List<recipesSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 1;
 
     private void Awake()
     {
+        waitingRecipeSOList = new List<recipesSO>();
+
         foreach (GameObject obj in hideIngredients)
         {
             obj.SetActive(false);
         }
     }
 
+    private void Update()
+    {
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer <= 0f)
+        {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+
+
+            if (waitingRecipeSOList.Count < waitingRecipesMax)
+            {
+                recipesSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
+                Debug.Log(waitingRecipeSO.recipeName);
+                waitingRecipeSOList.Add(waitingRecipeSO);
+            }
+        }
+    }
 
 
 }
